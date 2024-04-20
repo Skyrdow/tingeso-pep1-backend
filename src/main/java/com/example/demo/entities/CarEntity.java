4205ad9339f678296fc72ce642740df917ce0ef7
Skyrdow <lucas.mesias@usach.cl>
2024-04-19 22:18:25 -0400

    bug

--- src/main/java/com/example/demo/entities/CarEntity.java
+++ src/main/java/com/example/demo/entities/CarEntity.java
@@ -5,6 +5,7 @@ import com.example.demo.enums.MotorType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
